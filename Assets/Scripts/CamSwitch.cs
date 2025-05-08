ed76e5afee60c71bdd4082d52695f176cdd6e7bd
Benyamin ijaz <benyaminmahi@gmail.com>
2025-05-08 10:12:36 +0200

    gg

--- Assets/Scripts/CamSwitch.cs
+++ Assets/Scripts/CamSwitch.cs
@@ -1,22 +1,59 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CamSwitch : MonoBehaviour
 {
     public CinemachineCamera mainVCam;
-    public CinemachineCamera phoneVCam;
+    public CinemachineCamera tableVCam;
+    public CinemachineCamera mobileVCam;
+    public GameObject Button;
+    public GameObject PhoneScreen;
 
+
+    public void SwitchToTable()
+    {
+        mobileVCam.Priority = 2;
+        mainVCam.Priority = 5;
+        tableVCam.Priority = 10;
+    }
     public void SwitchToPhone()
     {
-        mainVCam.Priority = 5;
-        phoneVCam.Priority = 10;
+        mobileVCam.Priority = 10;
+        mainVCam.Priority = 2;
+        tableVCam.Priority = 5;
+        
+    }
+    public void SwitchToGround()
+    {
+        mainVCam.Priority = 10;
+        tableVCam.Priority = 5;
+        mobileVCam.Priority = 2;
     }
     private void Start()
     {
-        if (mainVCam!= null && phoneVCam != null)
+        if (mainVCam!= null && tableVCam != null && mobileVCam != null)
         {
             mainVCam.Priority = 10;
-            phoneVCam.Priority = 5;
+            tableVCam.Priority = 5;
+            mobileVCam.Priority = 2;
+
+            if (Button != null)
+            {
+                Button.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("Button not assigned!");
+            }
+            if (PhoneScreen != null)
+            {
+                PhoneScreen.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("PhoneScreen not assigned!");
+            }
         }
         else
         {
@@ -36,8 +73,51 @@ public class CamSwitch : MonoBehaviour
             {
                 if (hit.collider.CompareTag("MainDesk")) // or check tag
                 {
+                    if (Button != null)
+                    {
+                        Button.SetActive(true);
+                    }
+                    if (PhoneScreen != null)
+                    {
+                        PhoneScreen.SetActive(true);
+                    }
+                    else
+                    {
+                        Debug.LogError("PhoneScreen not assigned!");
+                    }
+                    SwitchToTable();
+                    
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(0)) // Left click
+        {
+            Ray ray = Camera.current.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.GetComponent("PhoneP")) // or check tag
+                {
                     SwitchToPhone();
                 }
+            }
+        }
+
+        if (Input.GetKeyDown("backspace"))// Left click
+        {
+            SwitchToGround();
+            if (Button != null)
+            {
+                Button.SetActive(false);
+            }
+            if (PhoneScreen != null)
+            {
+                PhoneScreen.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("PhoneScreen not assigned!");
             }
         }
     }
